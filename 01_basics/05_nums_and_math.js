0118f6d99ca2c0d4431fb1646baf8ea8c8eeea03
guru1121 <gurunandm@gmail.com>
2023-09-15 11:50:03 +0530

    math

---
+++ 01_basics/05_nums_and_math.js
@@ -0,0 +1,23 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ------------------------Maths-------------------
+
+const otherNumber = 28.85;
+
+// console.log(Math.round(otherNumber));
+
+
+
+console.log(Math.round((Math.random()*100)+1));
