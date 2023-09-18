6159a0efaa5830fccdb8fc1cfd402b99bcb37f79
guru1121 <gurunandm@gmail.com>
2023-09-18 11:00:02 +0530

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,19 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4];
+const fruits = [ 'banana', 'Apple', 'guava', 'Mango' ];
+
+myArr.push(5);
+// console.log(myArr);
+
+let a = myArr.concat(fruits);
+
+// console.log(a.includes('Apple'));
+// console.log(a.indexOf('banana'));
+
+const newArr = myArr.join();
+
+console.log(newArr);
+
+const myn1 = myArr; 
+console.log("a", myArr);
