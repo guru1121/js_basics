2a4e13007ee82dc361f99aeee2383c27ff2b09a9
guru1121 <gurunandm@gmail.com>
2023-09-18 16:37:36 +0530

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,6 +5,10 @@ const fruits = [ 'banana', 'Apple', 'guava', 'Mango' ];
 
 myArr.push(5);
 // console.log(myArr);
+// push function insert the value in last 
+//pop function removes last element
+//peek function 
+
 
 let a = myArr.concat(fruits);
 
@@ -13,7 +17,21 @@ let a = myArr.concat(fruits);
 
 const newArr = myArr.join();
 
-console.log(newArr);
+// console.log(newArr);
 
-const myn1 = myArr; 
+const myn1 = myArr.slice(0,3); 
+
+console.log(myn1);
 console.log("a", myArr);
+
+const myn2 = myArr.splice(1,3); 
+
+console.log(myn2);
+console.log("a", myArr);
+
+
+// here the slice() shows the data first to last-1 and 
+// all the values in the array are as it is 
+
+//in splice() shows the data all of the given index but it 
+//cut the value from array and that value substracted from the array
